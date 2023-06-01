26eb5d0f906645d8b59abec78ac3026e8e893d6a
ChicharronP <97786614+ChicharronP@users.noreply.github.com>
2023-05-31 17:10:49 -0700

    Delete inicializador.py

--- inicializador.py
+++
@@ -1,8 +0,0 @@
-class Inicializador:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        pygame.init()
-        self.surface = pygame.display.set_mode((self.width, self.height))
-        self.surface.fill((255, 255, 255))  # Color de fondo blanco
-        pygame.display.flip()
